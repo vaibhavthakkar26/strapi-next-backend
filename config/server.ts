0cb5ad4e4dc88e3d2b1cbef1ac28a6aa867a244b
vaibhav <vaibhavthakkar26051999@gmail.com>
2025-05-04 21:52:05 +0530

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('fe0bc5bf6b4f2b042b2556ff07880e6e'),
   },
 });
